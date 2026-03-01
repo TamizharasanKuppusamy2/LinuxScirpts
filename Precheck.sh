0ae325fb13b2f8c11eae6d7fa24806e8e2e8107b
Tamizharasan <tamizharasanwap@gmail.com>
2026-03-01 15:03:52 +0530

    Fixing errors

--- Precheck.sh
+++ Precheck.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
-
-
+#
 #####  This is a precheck script
 #####  Author : Tamizharasan K
 #####  This is for RHEL servers
-
+#
 echo "Server : $(hostname)"
 echo " "
 echo "Date : $(date)"
 echo " "
 echo "Uptime:"
 echo "------"
-    uptime
+    echo "$(uptime)"
 echo " "
 echo "Kernel Version:"
 echo "--------------"
-    uname -r
+    echo "$(uname -r)"
 echo " "
 echo "System Mount Points:"
 echo "--------------------"
-    df -hT | grep -v tmpfs
+    echo "$(df -hT | grep -v tmpfs)"
 echo " "
 echo "Route Details:"
 echo "--------------"
-    ip route
+    echo "$(ip route)"
 echo " "
 echo "Ip Details:"
 echo "-----------"
-    ip a
+    echo "$(ip a)"
 echo " "
-    yum clean all
+    echo "$(yum clean all)"
 echo "Repolist"
 echo "--------"
-    yum repolist
+    echo "$(yum repolist)"
 echo " "
 echo "Kernel Avaliablity:"
 echo "-------------------"
-    yum list kernel
+    echo "$(yum list kernel)"
 echo "Avaliable Patches:"
 echo "------------------"
-    yum updateinfo list sec
+    echo "$(yum updateinfo list sec)"
 echo " "
 echo "-------------------------------------------------The End-------------------------------------------------"
